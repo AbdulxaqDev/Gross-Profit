20065732ef9a04f24e53af13c442153f8205a86f
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-04-15 08:02:52 +0500

    add home intro image component

--- src/components/HomeIntro.vue
+++ src/components/HomeIntro.vue
@@ -1,13 +1,27 @@
 <script>
+import HomeIntroImg from "./HomeIntroImg.vue";
+export default {
+  components: {
+    HomeIntroImg: HomeIntroImg,
+  },
+};
 </script>
 
 <template>
   <div class="home-intro">
     <div class="home-intro-text">
-        <h1>Надежная<br><span class="yellow">БУХГАЛТЕРИЯ</span><br>для <span class="yellow">ВАШЕГО БИЗНЕСА</span></h1>
-        <p>Вы занимаетесь бизнесом, мы занимаемся Вашей бухгалтерией.<br>Ваша личная команда профессиональных бухгалтеров, которая всегда<br>под рукой.</p>
-        <a href=""><button>Оставить заявку</button></a>
+      <h1>
+        Надежная<br /><span class="yellow">БУХГАЛТЕРИЯ</span><br />для
+        <span class="yellow">ВАШЕГО БИЗНЕСА</span>
+      </h1>
+      <p>
+        Вы занимаетесь бизнесом, мы занимаемся Вашей бухгалтерией.<br />Ваша
+        личная команда профессиональных бухгалтеров, которая всегда<br />под
+        рукой.
+      </p>
+      <a href=""><button>Оставить заявку</button></a>
     </div>
+    <HomeIntroImg />
   </div>
 </template>
 
@@ -15,35 +29,39 @@
 .home-intro {
   height: 577px;
   padding: 0 50px 100px 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
 }
 
-.home-intro-text{
-    height: 100%;
-    width: 50vw;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: flex-start;
-    flex-direction: column;
+.home-intro-text {
+  height: 100%;
+  width: 50vw;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  flex-direction: column;
 }
 
-.home-intro-text h1{
-    color:  rgba(24, 32, 97, 1);
-    font-size: 48px;
+.home-intro-text h1 {
+  color: rgba(24, 32, 97, 1);
+  font-size: 48px;
 }
 
 .home-intro-text a {
   text-decoration: none;
   font-weight: 500;
 }
-.home-intro-text p{
-    color: rgba(62, 61, 61, 1);
-    font-weight: 400;
-    line-height: 21px;
-    font-size: 14px;
+.home-intro-text p {
+  color: rgba(62, 61, 61, 1);
+  font-weight: 400;
+  line-height: 21px;
+  font-size: 14px;
 }
 .home-intro-text button {
   background: rgba(244, 181, 4, 1);
-  color:#fff;
+  color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -65,8 +83,7 @@
 .home-intro-text a:hover button {
   color: rgba(244, 181, 4, 1);
 }
-.yellow{
-    color: rgba(244, 181, 4, 1);
+.yellow {
+  color: rgba(244, 181, 4, 1);
 }
-
 </style>
--- src/components/HomeIntroImg.vue
+++ src/components/HomeIntroImg.vue
@@ -1,10 +1,88 @@
 <script>
+import IntroMan from "../assets/HomeIntroImg/Group338.png";
 export default {};
 </script>
 
 <template>
-  <div class="home"></div>
+  <div class="homeIntroImg">
+    <div class="blueBlock1"></div>
+    <div class="blueBlock2"></div>
+    <img
+      class="introMan"
+      src="../assets/HomeIntroImg/Group338.png"
+      alt="You are welcome"
+    />
+    <img
+      class="progress"
+      src="../assets/HomeIntroImg/Group406.png"
+      alt="You are welcome"
+    />
+    <img
+      class="lupa"
+      src="../assets/HomeIntroImg/lupa1.png"
+      alt="You are welcome"
+    />
+  </div>
 </template>
 
 <style scoped>
+.homeIntroImg{
+    position: relative;
+    height: 100%;
+    width: 50%;
+}
+.blueBlock1 {
+  background: linear-gradient(
+    128.96deg,
+    rgba(24, 32, 97, 0.85) 10.45%,
+    rgba(255, 255, 255, 0.25) 116.98%
+  );
+  box-shadow: 0px 0px 50px rgba(237, 251, 255, 0.3);
+  transform: skewY(-8.04deg);
+  position: absolute;
+  width: 448.92px;
+  height: 379px;
+  left: 54.83px;
+  top: 111.57px;
+}
+
+.blueBlock2 {
+  position: absolute;
+  width: 360.17px;
+  height: 308.57px;
+  left: 212.96px;
+  top: 273.28px;
+
+  background: linear-gradient(
+    128.96deg,
+    rgba(24, 32, 97, 0.85) 10.45%,
+    rgba(255, 255, 255, 0.25) 116.98%
+  );
+  box-shadow: 0px 0px 50px rgba(237, 251, 255, 0.3);
+  transform: skewY(-8.04deg);
+  border-radius: 0 0 30px 0;
+}
+.introMan {
+  position: absolute;
+  width: 340px;
+  height: 577px;
+  left: 200px;
+  top: 10px;
+
+  background: url(image.png);
+  border-radius: 0px;
+}
+.lupa {
+  position: absolute;
+  left: 482px;
+  top: 144px;
+  bottom: 93.99%;
+}
+.progress {
+  position: absolute;
+  width: 88px;
+  height: 94px;
+  left: 522px;
+  top: 412px;
+}
 </style>
